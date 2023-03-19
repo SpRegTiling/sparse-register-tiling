98d8a8a1c8abb590c4ab58c00e75a4685474b353
Lucas Wilkinson <wilkinson.lucas@gmail.com>
2023-03-19 07:57:39 +0000

    no double on pi

--- cpp_testbed/demo/SPMM_demo.cpp
+++ cpp_testbed/demo/SPMM_demo.cpp
@@ -1063,10 +1063,12 @@ int main(int argc, char *argv[]) {
     if (scalar_type == "float") {
         auto experiment = SpMMExperiment<float>(root, { datasetDir, experimentDir }, overrides);
         result = experiment();
+#ifndef RASPBERRY_PI
     } else if (scalar_type == "double")  {
         std::cerr << "Waring double current unsupported by some baselines " << scalar_type << std::endl;
         auto experiment = SpMMExperiment<double>(root, { datasetDir, experimentDir }, overrides);
         result = experiment();
+#endif
     } else {
         std::cerr << "Unsupported scalar type " << scalar_type << std::endl;
     }
--- tools/paperv2/dlmc/dump_pi_rerun_script.py
+++ tools/paperv2/dlmc/dump_pi_rerun_script.py
@@ -9,12 +9,14 @@ from matplotlib import rc, rcParams
 from tools.paper.plotting.plot_utils import *
 from scipy import stats
 
-from tools.experiment_runner import *
 from tools import experiment_runner
 from tools.paper.configs import neon_nano_from_name
 
-experiment_runner.DATASET_DIR = DATASET_DIR
-os.makedirs("/tmp/experiment_scripts", exist_ok=True)
+experiment_runner.DATASET_DIR = "/home/pi/"
+experiment_runner.EXPERIMENTS_FOLDER = "/tmp/pi_rerun_scripts/yamls/"
+experiment_runner.CPP_BENCH_BINARY = "/home/pi/spmm-nano-bench/release-build/cpp_testbed/demo/SPMM_demo"
+os.makedirs("/tmp/pi_rerun_scripts/", exist_ok=True)
+os.makedirs("/tmp/pi_rerun_scripts/yamls/", exist_ok=True)
 
 adfs = []
 for bcol in [32, 128, 256, 512]:
@@ -34,13 +36,11 @@ baselines = [
 
 for bcols in [32, 128, 256, 512]:
     for threads in [1, 4]:
-        bcols = 32
-        threads = 1
-        outfile = RESULTS_DIR + f"/pi_rerun_{bcols}_{threads}.csv"
-        bash_script = f"/tmp/experiment_scripts/pi_rerun_{bcols}_{threads}.sh"
-        set_bash_script(bash_script)
+        outfile = f"/home/pi/pi_rerun_{bcols}_{threads}.csv"
+        bash_script = f"/tmp/pi_rerun_scripts/pi_rerun_{bcols}_{threads}.sh"
+        experiment_runner.set_bash_script(bash_script)
 
         df = filter(adf, Bcols=bcols, numThreads=threads)
         for index, row in adf.iterrows():
-            run_sp_reg_single(row['Bcols'], row['numThreads'], row["MatrixPath"], outfile,
+            experiment_runner.run_sp_reg_single(row['Bcols'], row['numThreads'], row["MatrixPath"], outfile,
                             "float", [neon_nano_from_name(row['orig_name|Sp. Reg.'], row['config|Sp. Reg.'])])
