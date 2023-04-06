7099a72b9fc7c2684e3fa506418f102e5a795498
Lucas Wilkinson <wilkinson.lucas@gmail.com>
2023-04-05 23:44:06 -0400

    More Cleanup

--- artifact/utils.py
+++
@@ -1,5 +0,0 @@
-import os
-
-RESULTS_DIR = os.getenv("RESULTS_DIR") + "/"
-PLOTS_DIR = os.getenv("PLOTS_DIR") + "/"
-
--- sync_to_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ..
-rsync -rltv --progress --exclude-from=spmm-nano-bench/rsync_exclude.txt spmm-nano-bench $1@niagara.computecanada.ca:/scratch/m/mmehride/$1/work_private
-cd spmm-nano-bench/
-
